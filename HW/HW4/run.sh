71ba0427a2f89b2c9edbc798e9af27cea91bc9d7
Jordan Van Duyne <jordannn808@gmail.com>
2015-10-28 15:24:23 -0400

    add run file

--- HW/HW4/run.sh
+++ HW/HW4/run.sh
@@ -1,2 +1,3 @@
-iverilog -o regfile.t.vvp -c file_list.txt
+# iverilog -o regfile.t.vvp -c file_list.txt
+iverilog -o regfile.t.vvp regfile.t.v regfile.v decoders.v register32.v mux32to1by32.v register32zero.v
 vvp regfile.t.vvp
